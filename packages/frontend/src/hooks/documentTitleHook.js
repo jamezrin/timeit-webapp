4a1f21bac1e2fff0023270c589bf35328c981f79
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2020-11-21 16:37:48 +0100

    Make custom useDocumentTitle hook reset initial title

--- packages/frontend/src/hooks/documentTitleHook.js
+++ packages/frontend/src/hooks/documentTitleHook.js
@@ -1,7 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function useDocumentTitle(title) {
+  const initialTitle = useRef(document.title);
+
   useEffect(() => {
     document.title = title;
   }, [title]);
+
+  return () => {
+    document.title = initialTitle.current;
+  };
 }
