4fe082458ecef600ad94b69756a835f3e09c823a
Jaime Martínez Rincón <jaime@jamezrin.name>
2021-10-24 20:20:48 +0200

    Show migration status on backend startup via image

--- packages/backend/docker-entrypoint.sh
+++ packages/backend/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "=== Current migration status:"
+yarn workspace backend typeorm migration:show
+
 if [ -n "$TIMEIT_RUN_MIGRATIONS" ]; then
   echo "=== Running typeorm migrations..."
   yarn workspace backend typeorm migration:run
