974caf0838451f24237ab3677d052019fe1fcb15
Jaime Martínez Rincón <jaime@jamezrin.name>
2021-10-27 22:12:31 +0200

    Fix backend docker entrypoint for rush deploy

--- apps/backend/docker-entrypoint.sh
+++ apps/backend/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 echo "=== Current migration status:"
-yarn workspace @timeit/backend typeorm migration:show
+npx typeorm migration:show
 
 if [ -n "$TIMEIT_RUN_MIGRATIONS" ]; then
   echo "=== Running typeorm migrations..."
-  yarn workspace @timeit/backend typeorm migration:run
+  npx typeorm migration:run
 fi
 
 echo "=== Starting the server..."
-node /app/packages/backend/dist/index.js
+npm run start
